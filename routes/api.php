6cc68fba2730f1e42266206cc5b33c6d518ff735
fmx <1417388068@qq.com>
2018-07-22 03:47:56 +0800

    Api-auth

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,48 @@ use App\Http\Resources\Product as ProductResource;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Route::post('login', 'Auth\LoginController@ApiLogin');
+Route::post('login', function (Request $request) {
+
+    if (auth()->attempt(['email' => $request->input('email'), 'password' => $request->input('password')])) {
+        // Authentication passed...
+        $user = auth()->user();
+        $user->api_token = str_random(60);
+        $user->save();
+        return $user;
+    }
+
+    return response()->json([
+        'error' => 'Unauthenticated user',
+        'code' => 401,
+    ], 401);
+});
+
+Route::middleware('auth:api')->post('logout', function (Request $request) {
+
+    if (auth()->user()) {
+        $user = auth()->user();
+        $user->api_token = null; // clear api token
+        $user->save();
+
+        return response()->json([
+            'message' => 'Thank you for using our application',
+        ]);
+    }
+
+    return response()->json([
+        'error' => 'Unable to logout user',
+        'code' => 401,
+    ], 401);
+});
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
@@ -59,5 +101,14 @@ Route::get('distributors', function () {
 
 Route::get('foods', function () {
     return FoodResource::collection(Food::all());
+});
+
+Route::middleware('auth:api')->get('test', function() {
+//    if (Auth::guest()) {
+//        return 'guest';
+//    }
+//    return 'otherwise';
+
+    return Auth::user();
 });
 
